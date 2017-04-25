5a7252748715e54ed4cdab362a8bdcdf1b8cb0ca
tatomyr <andrew.tatomyr@gmail.com>
2017-04-25 23:07:32 +0300

    fixed bug with mobile IE: it was for-of loop

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,6 @@ class App extends React.Component {
   handleSubmit(e) {
     // console.clear();
 
-    document.getElementById('progress').innerHTML += ` 1 `;
 
     const service = e.target.service.value.toString();
     const salt = e.target.salt.value.toString();
@@ -30,23 +29,18 @@ class App extends React.Component {
     localStorage.pwLength = pwLength;
 
     if (service && salt && pwLength) {
-      document.getElementById('progress').innerHTML += ` 2 `;
       console.log('1>----------');
       this.setState({ displaySpinner: 'block' });
       console.log('2>----------');
       setTimeout(() => {
-        document.getElementById('progress').innerHTML += ` 3 `;
         this.getRecurrPw(service, salt, pwLength, (password) => {
           this.setState({ password });
-          document.getElementById('progress').innerHTML += ` 4 `;
           this.refs.password.select();
           console.log('copied:',document.execCommand('copy'));
 
           if (document.execCommand('copy')) {
             // Doesn't work in Firefox
-            document.getElementById('progress').innerHTML += ` 5 `;
             setTimeout(() => {
-              document.getElementById('progress').innerHTML += ` 6 `;
               this.setState({ password: '' });
               document.getElementById('service').focus();
             }, 1000);
@@ -60,15 +54,13 @@ class App extends React.Component {
   }
 
   getRecurrPw(service, salt, pwLength, callback) {
-    document.getElementById('progress').innerHTML += ` 3:1 `;
 
     const password = this.pw3(service, salt, pwLength);
-    document.getElementById('progress').innerHTML += ` 3:2 `;
 
     let upperCasedCount = 0;
     let lowerCasedCount = 0;
     let numbersCount = 0;
-    /*
+    /* // Mobile IE doesn't support this
     for (let val of password) {
       numbersCount += !isNaN(+val) && 1 || 0;
       upperCasedCount += val.toUpperCase() === val && isNaN(+val) && 1 || 0;
@@ -82,39 +74,31 @@ class App extends React.Component {
     	lowerCasedCount += val.toLowerCase() === val && isNaN(+val) && 1 || 0;
     }
 
-    document.getElementById('progress').innerHTML += ` 3:3 `;
 
     console.log('--->',  numbersCount,upperCasedCount, lowerCasedCount,)
     if (numbersCount >= 2 && upperCasedCount >= 1 && lowerCasedCount >= 1) {
       this.setState({ displaySpinner: 'none' });
-      document.getElementById('progress').innerHTML += ` 3:4u `;
 
       return callback(password);
     } else {
-      document.getElementById('progress').innerHTML += ` 3:4d `;
 
       return this.getRecurrPw(password, salt, pwLength, callback);
     }
   }
 
   pw3(service_, salt_, pwLength) {
-    document.getElementById('progress').innerHTML += ` pw:1 `;
 
     let service = service_.toString();
-    document.getElementById('progress').innerHTML += ` pw:a `;
     if (service.length === 1) {
       service += this.pwChar(service.charCodeAt());
     }
-    document.getElementById('progress').innerHTML += ` pw:b `;
     let salt = salt_.toString();
-    document.getElementById('progress').innerHTML += ` pw:c `;
     if (!((salt.length + 1) % service.length && service.length % (salt.length + 1))) {
       salt += this.pwChar((salt.length + service.length));
     }
-    document.getElementById('progress').innerHTML += ` pw:d `;
 
     let sum = 0;
-    /*
+    /* // Mobile IE doesn't support this
     for (let item of service) {
       sum += item.charCodeAt();
     }
@@ -129,17 +113,13 @@ class App extends React.Component {
       sum += salt.charCodeAt(k);
     }
 
-    document.getElementById('progress').innerHTML += ` pw:e `;
     const jOffset = sum % service.length;
     const kOffset = sum % (salt.length + 1);
-    document.getElementById('progress').innerHTML += ` pw:f `;
 
     // I don't know why, but Array(pwLength).fill(0) doesn't create an array properly (??)
     const pwArr = [];
-    document.getElementById('progress').innerHTML += ` pw:g `;
     console.log(service, jOffset, salt, kOffset, pwArr, pwLength);
 
-    document.getElementById('progress').innerHTML += ` pw:2 `;
 
 
     for (let i = 0; !(i >= pwLength && i >= service.length && i >= (salt.length + 1)); i++) {
@@ -163,10 +143,8 @@ class App extends React.Component {
       );
 
     }
-    document.getElementById('progress').innerHTML += ` pw:3 `;
 
     const password = pwArr.reduce((prev, item) => prev + this.pwChar(item), '');
-    document.getElementById('progress').innerHTML += ` pw:4 `;
     console.log(password);
     return password;
   }
@@ -210,7 +188,6 @@ class App extends React.Component {
         </div>
         <Banner />
         <Spinner display={this.state.displaySpinner} />
-        <div id="progress">=</div>
       </div>
     );
   }
