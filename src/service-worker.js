d5afca4330e4498b9676aba8997c94962a9903dc
tatomyr <andrew.tatomyr@gmail.com>
2019-04-26 15:16:59 +0300

    Add service worker; improve build script; fix grammar issues in info; update tests

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,31 +1,38 @@
 /* eslint-disable no-restricted-globals, no-console */
-const cacheName = 'password-generator'
-const filesToCache = ['/', '/index.html', '/app.js', '/style.css', '/key-512.svg']
+const dev = location.hostname === 'localhost'
+console.log(`Built date: __BUILD_DATE__, ${dev ? 'development mode' : 'production mode'}`)
 
-self.addEventListener('install', e => {
-  console.log('[ServiceWorker] Install')
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)))
-})
+if (dev) {
+  console.log('[ServiceWorker] SKIPPED DUE TO DEVELOPMENT MODE')
+} else {
+  const cacheName = 'password-generator'
+  const filesToCache = ['/', '/index.html', '/app.js', '/style.css', '/key-512.svg']
 
-self.addEventListener('activate', e => {
-  console.log('[ServiceWorker] Activate')
-  e.waitUntil(
-    caches.keys().then(keyList => Promise.all(
-      // FIXME: linter errors
-      // eslint-disable-next-line array-callback-return, consistent-return
-      keyList.map(key => {
-        if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key)
-          return caches.delete(key)
-        }
-      })
-    ))
-  )
+  self.addEventListener('install', e => {
+    console.log('[ServiceWorker] Install')
+    e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)))
+  })
 
-  return self.clients.claim()
-})
+  self.addEventListener('activate', e => {
+    console.log('[ServiceWorker] Activate')
+    e.waitUntil(
+      caches.keys().then(keyList => Promise.all(
+        // FIXME: linter errors
+        // eslint-disable-next-line array-callback-return, consistent-return
+        keyList.map(key => {
+          if (key !== cacheName) {
+            console.log('[ServiceWorker] Removing old cache', key)
+            return caches.delete(key)
+          }
+        })
+      ))
+    )
 
-self.addEventListener('fetch', e => {
-  console.log('[Service Worker] Fetch', e.request.url)
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)))
-})
+    return self.clients.claim()
+  })
+
+  self.addEventListener('fetch', e => {
+    console.log('[Service Worker] Fetch', e.request.url)
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)))
+  })
+}
