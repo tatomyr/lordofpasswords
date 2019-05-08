90c2a64f95cdc32ac1783297f63b16514c82d0a8
tatomyr <andrew.tatomyr@gmail.com>
2019-05-09 02:17:18 +0300

    Implement css postprocessing

--- bin/build.sh
+++ bin/build.sh
@@ -5,6 +5,9 @@ rm -rf dist/
 mkdir dist
 cp -R src/* dist/
 
+# Processing CSS
+postcss src/style.css -o dist/style.css --no-map -u autoprefixer -u cssnano
+
 # Changing build date to refresh the service worker
 file_content=`cat ./src/service-worker.js`
 output="${file_content/__BUILD_DATE__/$(date)}"
