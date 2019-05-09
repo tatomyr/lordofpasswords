a97609e206046cec9a07c9505330ae2ccdc55b36
tatomyr <andrew.tatomyr@gmail.com>
2019-05-09 15:32:54 +0300

    Fix vulnerabilities: replace http-server with static-server

--- bin/test.sh
+++ bin/test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Start testing server
-cd src && http-server -p 8081 &
+cd src && static-server -p 8081 &
 # Run tests with Cypress
 cypress run --config baseUrl=http://localhost:8081
 # Catch the most recent command response
 RESULT=$?
 # Find the testing server PID
-test_server_PID=`ps -A | grep 'http-server -p 8081' | grep -v grep | awk '{print $1}'`
+test_server_PID=`ps -A | grep 'static-server -p 8081' | grep -v grep | awk '{print $1}'`
 # Kill the process
 kill -9 $test_server_PID
 # Exit with error if testing has failed
