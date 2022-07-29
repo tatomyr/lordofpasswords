dd9e90ee176841d7bc83f78dcd2ea1a3843f9414
Andrew Tatomyr <andrew.tatomyr@gmail.com>
2022-07-29 09:50:05 +0300

    fix: eliminate deadlock in serviceworker (#17)

--- lordofpasswords.sw.template.js
+++ lordofpasswords.sw.template.js
@@ -4,7 +4,7 @@ console.log('Build date: __BUILD_DATE__')
 
 const APP_SCOPE = 'lordofpasswords'
 const cacheName = `${APP_SCOPE}@__VERSION__`
-const FILES_TO_CASHE = [
+const FILES_TO_CACHE = [
   './',
   './index.html',
   './app.js',
@@ -27,22 +27,26 @@ const FILES_TO_CASHE = [
 self.addEventListener('install', (e) => {
   console.log('[lordofpasswords.sw] Install')
   e.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(FILES_TO_CASHE))
+    caches.open(cacheName).then((cache) => cache.addAll(FILES_TO_CACHE))
   )
 })
+
+// TODO: unregister other service-workers?
 
 self.addEventListener('activate', (e) => {
   console.log('[lordofpasswords.sw] Activate')
   e.waitUntil(
-    caches.keys().then((keyList) => Promise.all(
-      // eslint-disable-next-line array-callback-return, consistent-return
-      keyList.map((key) => {
-        if (key !== cacheName) {
-          console.log('[lordofpasswords.sw] Removing old cache', key)
-          return caches.delete(key)
-        }
-      })
-    ))
+    caches.keys().then((keyList) =>
+      Promise.all(
+        // eslint-disable-next-line array-callback-return, consistent-return
+        keyList.map((key) => {
+          if (key !== cacheName) {
+            console.log('[lordofpasswords.sw] Removing old cache', key)
+            return caches.delete(key)
+          }
+        })
+      )
+    )
   )
   console.log(self.clients)
   return self.clients.claim()
@@ -51,11 +55,57 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', (e) => {
   console.log('[lordofpasswords.sw] Fetch', e.request.url)
   e.respondWith(
-    caches
-      .match(e.request)
-      // FIXME: do not return Locked status since cache is not guaranteed to live forever
-      .then(
-        (res) => res || new Response('Locked', { status: 423, statusText: 'Locked' })
-      )
+    caches.open(cacheName).then((cache) =>
+      cache
+        .match(e.request)
+        .then((cachedResponse) => {
+          if (cachedResponse) {
+            // If there is an entry in the cache for e.request, then cachedResponse will be defined
+            // and we can just return it. Note that in this example, only font resources are cached.
+
+            return cachedResponse
+          }
+
+          // Otherwise, if there is no entry in the cache for e.request, response will be
+          // undefined, and we need to fetch() the resource.
+          console.warn(
+            ' No response for %s found in cache. About to fetch from network…',
+            e.request.url
+          )
+
+          const precachedFiles = FILES_TO_CACHE.map(
+            (relativePath) =>
+              new URL(relativePath, self.registration.scope).href
+          )
+          if (!precachedFiles.includes(e.request.url)) {
+            return new Response('Locked', { status: 423, statusText: 'Locked' })
+          }
+
+          // We call .clone() on the request ...
+          // ...since we might use it in a call to cache.put() later on.
+          // Both fetch() and cache.put() "consume" the request, so we need to make a copy.
+          // (see https://developer.mozilla.org/en-US/docs/Web/API/Request/clone)
+          return fetch(e.request.clone()).then((response) => {
+            if (response.ok) {
+              console.warn('  Caching the response to', e.request.url)
+              cache.put(e.request, response.clone())
+            } else {
+              console.error('  Not caching the response to', e.request.url)
+            }
+
+            // Return the original response object, ...
+            // ...which will be used to fulfill the resource request.
+            return response
+          })
+        })
+        .catch((err) => {
+          // This catch() will handle exceptions that arise from the match() or fetch() operations.
+          // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
+          // It will return a normal response object that has the appropriate error code set.
+          console.error('  Error in fetch handler:', err)
+
+          throw err
+        })
+    )
   )
 })
